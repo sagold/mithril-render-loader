fa398afef5b4ef2beeaad4a78ceb1afaac78f0ce
Sascha Goldhofer <post@saschagoldhofer.de>
2017-10-23 11:37:58 +0200

    Update options

--- index.js
+++ index.js
@@ -26,6 +26,16 @@ function logTime(message, startTime, endTime) {
 function mithrilRenderLoader(view) {
     const timeStart = Date.now();
 
+    // options
+    const o = Object.assign({
+        profile: false, // log build times
+        model: null, // data passed to component
+        "export": false,  // use module.exports or return result as string (html-loader)
+        // vm
+        whitelist: ["fs", "path", "module"], // whitelist native modules, "*" is also possible
+        sandbox: {} // environment variables and helpers within nodejs-context of component
+    }, this.query);
+
     // the render-mithril operation is async
     var done = this.async();
 
@@ -36,11 +46,10 @@ function mithrilRenderLoader(view) {
     // configure the vm which will run the given contents
     const vm = new vm2.NodeVM({
         console: "inherit",
-        sandbox: {
-        },
+        sandbox: Object.assign({}, o.sandbox),
         require: {
             external: true,
-            builtin: ["fs", "path", "module"],
+            builtin: o.whitelist,
             root: "./",
             mock: {
                 exportRequires(list) {
@@ -53,7 +62,7 @@ function mithrilRenderLoader(view) {
     });
 
     const timeStartRun = Date.now();
-    this.query.profile && logTime("initializing vm", timeStart, timeStartRun);
+    o.profile && logTime("initializing vm", timeStart, timeStartRun);
 
 
     // run the contents in nodejs, hooking into require
@@ -65,7 +74,7 @@ function mithrilRenderLoader(view) {
     }
 
     const startRendering = Date.now();
-    this.query.profile && logTime(`executing ${this.resource}`, timeStartRun, startRendering);
+    o.profile && logTime(`executing ${this.resource}`, timeStartRun, startRendering);
 
 
     dependencies.forEach((filepath) => {
@@ -76,12 +85,12 @@ function mithrilRenderLoader(view) {
     });
 
     // fetch the required data and render the component
-    render(m(view, this.query.model)).then((html) => {
+    render(m(view, o.model)).then((html) => {
         const timeEnd = Date.now();
-        this.query.profile && logTime("rendering component", startRendering, timeEnd);
-        this.query.profile && logTime(`total time ${this.resource}`, timeStart, timeEnd);
+        o.profile && logTime("rendering component", startRendering, timeEnd);
+        o.profile && logTime(`total time ${this.resource}`, timeStart, timeEnd);
 
-        if (this.query.export) {
+        if (o.export) {
             // if no html loader
             return done(null, `module.exports = ${JSON.stringify(html)}`);
         }
