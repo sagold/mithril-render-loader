df67b492a44d47c689fdfe3cc9f2b1b06ae02ea4
Sascha Goldhofer <post@saschagoldhofer.de>
2017-10-24 09:59:00 +0200

    Change rendering and dependency retrieval

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ require("mithril/test-utils/browserMock")(global);
 const m = require("mithril");
 const chalk = require("chalk");
 const render = require("mithril-node-render");
-const vm2 = require("vm2");
-
-
-const requireHook = `
-    var path = require("path");
-    var hook = require("require-hook");
-    hook.attach(path.resolve());
-`;
-
-const requireExport = `
-    require('exportRequires')(hook.getData());
-    hook.detach(path.resolve());
-`;
 
 
 function logTime(message, startTime, endTime) {
@@ -25,79 +12,88 @@ function logTime(message, startTime, endTime) {
 
 function mithrilRenderLoader(view) {
     const timeStart = Date.now();
-
+    // the render-mithril operation is async
+    var done = this.async();
     // options
     const o = Object.assign({
         profile: false, // log build times
         model: null, // data passed to component
-        "export": false,  // use module.exports or return result as string (html-loader)
-        // vm
-        whitelist: ["fs", "path", "module"], // whitelist native modules, "*" is also possible
-        sandbox: {} // environment variables and helpers within nodejs-context of component
+        "export": false // use module.exports or return result as string (html-loader)
     }, this.query);
 
-    // the render-mithril operation is async
-    var done = this.async();
-
-    // put required dependencies here
-    let dependencies;
-
-
-    // configure the vm which will run the given contents
-    const vm = new vm2.NodeVM({
-        console: "inherit",
-        sandbox: Object.assign({}, o.sandbox),
-        require: {
-            external: true,
-            builtin: o.whitelist,
-            root: "./",
-            mock: {
-                exportRequires(list) {
-                    dependencies = list
-                        .map((info) => info.absPath)
-                        .filter((path) => typeof path === "string");
+    // prototype - webpack require
+    const requests = [];
+    const self = this;
+    function resolveModule(id, requestPath) {
+        return new Promise((resolve, reject) => {
+            // console.log(`mithril require ${requestPath}`);
+            self.loadModule(requestPath, (err, source, sourceMap, module) => {
+                if (err) {
+                    return reject(err);
                 }
-            }
-        }
-    });
-
-    const timeStartRun = Date.now();
-    o.profile && logTime("initializing vm", timeStart, timeStartRun);
-
-
-    // run the contents in nodejs, hooking into require
-    try {
-        view = vm.run(`${requireHook}${view}${requireExport}`, this.resource);
-    } catch (e) {
-        done(e);
-        return;
+                // module.exports = __webpack_public_path__ + "intro-mobile-f825065fb480b357.jpg";
+                source = source.replace(/^module.exports[^"]*/, "").replace(/(^"|";$)/g, "");
+                return resolve({ id, source });
+            });
+        });
     }
 
-    const startRendering = Date.now();
-    o.profile && logTime(`executing ${this.resource}`, timeStartRun, startRendering);
+    global.resolve = function (requestPath) {
+        const id = `{{${requestPath}${Date.now()}${Math.random()}`;
+        requests.push(resolveModule(id, requestPath));
+        return id;
+    };
 
 
-    dependencies.forEach((filepath) => {
-        // watch file dependencies
-        this.addDependency(filepath);
-        // and remove them from cache in order to be reread next run (webpack-dev-server)
-        delete require.cache[filepath];
-    });
+    const dependenciesBefore = Object.keys(require.cache);
+    view = require(this.resourcePath);
+    let timeResolve;
 
     // fetch the required data and render the component
-    render(m(view, o.model)).then((html) => {
-        const timeEnd = Date.now();
-        o.profile && logTime("rendering component", startRendering, timeEnd);
-        o.profile && logTime(`total time ${this.resource}`, timeStart, timeEnd);
+    render(m(view, o.model))
+        .then((html) => {
+            timeResolve = Date.now();
+            o.profile && logTime(`render ${this.resource}`, timeStart, timeResolve);
+            global.resolve = undefined;
 
-        if (o.export) {
-            // if no html loader
-            return done(null, `module.exports = ${JSON.stringify(html)}`);
-        }
+            const dependencies = [];
+            Object.keys(require.cache).forEach((filepath) => {
+                if (dependenciesBefore.indexOf(filepath) === -1) {
+                    dependencies.push(filepath);
+                }
+            });
 
-        return done(null, html);
-    })
-    .catch(done);
+            // clear cache AFTER main file has been compiled
+            dependencies.forEach((filepath) => {
+                // watch file dependencies
+                this.addDependency(filepath);
+                // and remove them from cache in order to be reread next run (webpack-dev-server)
+                delete require.cache[filepath];
+            });
+
+
+            return html;
+        })
+        .then((html) => Promise
+            .all(requests)
+            .then((results) => {
+                o.profile && logTime(`resolve ${this.resource}`, timeResolve, Date.now());
+
+                results.forEach((data) => {
+                    html = html.replace(data.id, data.source);
+                });
+                return html;
+            })
+        )
+        .then((html) => {
+            o.profile && logTime(`total time ${this.resource}`, timeStart, Date.now());
+
+            if (o.export) { // if no html loader
+                return done(null, `module.exports = ${JSON.stringify(html)}`);
+            }
+            return done(null, html);
+        })
+        .catch(done);
 }
 
 
